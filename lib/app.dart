5893f7c2b54d52840c7b56b193dd272ba681e229
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2024-12-12 22:09:47 +0545

    feat : 3 Onboaring screen (#5)
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:orenda/views/onboarding_screen_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:orenda/views/login_view.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +10,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: AppStarter(),
     );
+  }
+}
+
+class AppStarter extends StatefulWidget {
+  const AppStarter({super.key});
+
+  @override
+  State<AppStarter> createState() => _AppStarterState();
+}
+
+class _AppStarterState extends State<AppStarter> {
+  bool? _firstLaunch;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkFirstLaunch();
+  }
+
+  Future<void> _checkFirstLaunch() async {
+    final prefs = await SharedPreferences.getInstance();
+    final firstLaunch = prefs.getBool('firstLaunch') ?? true;
+
+    setState(() {
+      _firstLaunch = firstLaunch;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_firstLaunch == null) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    return _firstLaunch! ? const OnboardingScreen() : const Login();
   }
 }
