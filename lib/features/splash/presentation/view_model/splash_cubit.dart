deae415159b73a0c11c187e01eac743b54ee0fb8
abhisekthapam <ahisekmagar250203@gmail.com>
2025-03-04 20:23:31 +0545

    fix : 2 Bug fixes in onboarding

--- lib/features/splash/presentation/view_model/splash_cubit.dart
+++ lib/features/splash/presentation/view_model/splash_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:orenda/features/auth/presentation/view/login_view.dart';
+import 'package:orenda/app/shared_prefs/onboarding_screen_view.dart';
 import 'package:orenda/features/auth/presentation/view_model/login/login_bloc.dart';
 
 class SplashCubit extends Cubit<void> {
@@ -10,15 +10,13 @@ class SplashCubit extends Cubit<void> {
 
   Future<void> init(BuildContext context) async {
     await Future.delayed(const Duration(seconds: 2), () async {
-      // Open Login page or Onboarding Screen
-
       if (context.mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
             builder: (context) => BlocProvider.value(
               value: _loginBloc,
-              child: LoginView(),
+              child: OnboardingScreen(),
             ),
           ),
         );
